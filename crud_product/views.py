6ea2f36d2728c3f0f7a65bd695ed8d957ceec56b
Farzana Hadifa <hadifafarzana@gmail.com>
2022-05-13 17:51:15 +0700

    ubah implementasi API

--- crud_product/views.py
+++ crud_product/views.py
@@ -2,13 +2,19 @@ from rest_framework.exceptions import ValidationError
 from rest_framework import generics
 from .serializers import ProductSerializer
 from .models import Product
+from rest_framework.response import Response
 
 class ProductList(generics.ListCreateAPIView):
     queryset =  Product.objects.all()
     serializer_class = ProductSerializer
 
-    def perform_create(self, serializer):
-        serializer.save()
+    def create(self, request,  *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response({'status':'success','message':'Produk berhasil dibuat', 'produk':serializer.data},
+                             status=201)
+        return Response(serializer.errors, status=400)
 
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -16,8 +22,15 @@ class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         return Product.objects.all()
 
-    def perform_update(self, serializer):
-        serializer.save()
+    def update(self, request,  *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            self.perform_update(serializer)
+            return Response({'status':'success','message':'Produk berhasil diubah', 'produk':serializer.data})
+        return Response(serializer.errors, status=400)
 
-    def perform_destroy(self, instance):
-        instance.delete()
+    def destroy(self, request,  *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({'status':'success','message':'Produk berhasil dihapus'})
